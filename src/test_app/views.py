13024c124f8ff1fa377ce3edb6d21046e0733587
Kris <userfox316@gmail.com>
2022-07-29 18:41:42 +0300

    user registration

--- src/test_app/views.py
+++ src/test_app/views.py
@@ -1,10 +1,15 @@
-from django.contrib.auth import authenticate, logout, login
+from django.contrib.auth import authenticate, logout, login, get_user_model
+from django.contrib.auth.tokens import default_token_generator
 from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render, redirect
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.views.generic import UpdateView, ListView, DetailView
 from test_app.email import send
 from test_app.models import Subject, Person
+
+USER_MODEL = get_user_model()
 
 
 def index_template(request):
@@ -54,7 +59,6 @@ class PersonDjangoDetailView(DetailView):
 
 
 def signin(request):
-
     if request.method == "GET":
         return render(request, "login.html")
 
@@ -74,3 +78,50 @@ def signin(request):
 def signout(request):
     logout(request)
     return redirect(reverse_lazy("login"))
+
+
+def signup(request):
+    if request.method == "GET":
+        return render(request, "registration/signup.html")
+
+    elif request.method == "POST":
+        email = request.POST.get("email")
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = USER_MODEL.objects.create_user(
+            username=username,
+            email=email,
+            password=password,
+            is_active=False
+        )
+
+        send(
+            subject='Verify your account',
+            to_email=email,
+            template_name='registration/verify_account',
+            context={
+                "username": username,
+                "verify_url": reverse('verify_account', kwargs={
+                    "uid": urlsafe_base64_encode(force_bytes(user.pk)),
+                    "token": default_token_generator.make_token(user)
+                }),
+                "request": request
+            }
+        )
+
+        return HttpResponse(f"Hello {user.username}. You need to verify your account")
+
+
+def verify_account(request, uid, token):
+    try:
+        user = USER_MODEL.objects.get(pk=urlsafe_base64_decode(uid))
+    except():
+        user = None
+
+    if user is not None and default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        return HttpResponse(f'Hello, {user.username}. Your account is verified')
+    else:
+        return HttpResponse('Invalid link')
